a30bb03794d45f3d174151d0689ee3b03b480c91
Aaron Tonatiuh Villanueva Guzmán <30881287+AaronVillanueva@users.noreply.github.com>
2019-10-13 20:54:53 -0500

    Update sudoku.c

--- sudoku.c
+++ sudoku.c
@@ -6,23 +6,38 @@
 /*1 vertical, 1 horizontals, 9 for subgrids*/
 typedef struct data{
   int num;
-  int array[9]
 }
+
+void *column(void *arg);
+void *row(void *arg);
+void *subgrid(void *arg);
 
 int main(){
   
-  int sudoku[9][9]={ {0,0,0,0,0,0,0,0,0},
-                    {0,0,0,0,0,0,0,0,0},
-                    {0,0,0,0,0,0,0,0,0},
-                    {0,0,0,0,0,0,0,0,0},
-                    {0,0,0,0,0,0,0,0,0},
-                    {0,0,0,0,0,0,0,0,0},
-                    {0,0,0,0,0,0,0,0,0},
-                    {0,0,0,0,0,0,0,0,0},
-                    {0,0,0,0,0,0,0,0,0}};
+  int sudoku[9][9]={{8,3,5,4,1,6,9,2,7},
+                    {2,9,6,8,5,7,4,3,1},
+                    {4,1,7,2,9,3,6,5,8},
+                    {5,6,9,1,3,4,7,8,2},
+                    {1,2,3,6,7,8,5,4,9},
+                    {7,4,8,5,2,9,1,6,3},
+                    {6,5,2,7,8,1,3,9,4},
+                    {9,8,1,3,4,5,2,7,6},
+                    {3,7,4,9,6,2,8,1,5}};
+  
   int results[11];
   
-
+ 
+  data datC,datR;
+  pthread_t threads[11];
+  
+  datC.num=0;
+  pthread_create(&threads[0],NULL,column,(void *)datC);
+  
+  
+  
+  pthread_join(threads[0]);
+  printf(results[0]);
+  
   return 0;
 }
 
@@ -49,7 +64,12 @@ void column(void *arg){
   }
   }
   
-  if (count==9)
+  if (count==9){
+    results[num]=1;
+  }
+  else{
+    results[num]=0;
+  }
     
   
 }
